0adf215a9f238622c3ad3361d410c6d15b504fd6
leenusdarnal <leenus.darnal@gmail.com>
2023-10-28 15:08:37 +0530

    added favicon and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import '../app.css'
 </script>
 
+<svelte:head>
+	<title>This is the Official Site of ROFC Sikkim</title>
+</svelte:head>
 <div class="font- min-h-screen bg-black p-1 text-white">
 	<Navbar />
 	<slot />
